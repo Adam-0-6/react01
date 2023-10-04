e645af0ab30f1fce8c3c876bc4fc821fcd8038b2
Adam Estoev <adamestoev2001@gmail.com>
2023-10-04 18:59:03 +0300

    first

---
+++ src/components/Content.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Content = () =>{
+    return (
+        <main className='content'>
+            <div>
+            <img src='https://img.goodfon.ru/original/1024x768/5/9b/afrika-lev-retush-dikaya.jpg' alt='img'/>
+            </div>
+            <div>   
+            <p>my post</p>
+            <img src='http://lh3.googleusercontent.com/vZhF7N5Cy2skf72gMfV_lfgoxB_bqEF5DKqQN6rI9s0gpsR3ZVtZtdKnqAYoP41QcdQEOE84GtRflRFTJHtQ09wLmfvqDHNt' alt='img' />
+            </div>
+        </main>
+    )
+}
+
+export default Content;
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () =>{
+    return(
+        <header className='header'>
+            <img width={60} height={60} src='https://forum.4story.gameforge.com/images/avatars/da/1426-da08a0447754c013651dee385e8255857fc3f50d.png' alt='LOGO' />
+        </header>
+    )
+}
+    
+export default Header;
